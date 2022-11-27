0d071e3d2791bc91fbd070cfb7b5deb4d3327ff8
ToniPortal <tonitoctoc13@gmail.com>
2022-11-27 23:01:47 +0100

    envoie

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const {
     ipcMain,
     nativeTheme
 } = require('electron')
+
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 const path = require('path')
+
 
 
 function createWindow() {
@@ -12,17 +16,68 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            enableRemoteModule: true
+
         }
     })
 
     win.loadFile('./web/index.html')
 
 
+    ipcMain.on('servinfo', async (event, arg) => {
+
+        var yti = await ytdl.getBasicInfo(arg)
+        console.log(`idytb: ${yti.videoDetails.videoId}\nTitre: ${yti.videoDetails.title}\nNom de la chaîne: ${yti.videoDetails.ownerChannelName}\nDate d'upload: ${yti.videoDetails.uploadDate}`)
+        event.returnValue = `idytb: ${yti.videoDetails.videoId}\nTitre: ${yti.videoDetails.title}\nNom de la chaîne: ${yti.videoDetails.ownerChannelName}\nDate d'upload: ${yti.videoDetails.uploadDate}`;
+
+    });
+
+
+
+
+    win.webContents.session.on('will-download', (event, item, webContents) => {
+        // Set the save path, making Electron not to prompt a save dialog.
+
+        item.on('updated', (event, state) => {
+            if (state === 'interrupted') {
+                console.log('Download is interrupted but can be resumed')
+            } else if (state === 'progressing') {
+                if (item.isPaused()) {
+                    console.log('Download is paused')
+                } else {
+                    console.log(`Received bytes: ${item.getReceivedBytes()}`)
+                }
+            }
+        })
+        item.once('done', (event, state) => {
+            if (state === 'completed') {
+                console.log('Download successfully')
+            } else {
+                console.log(`Download failed: ${state}`)
+            }
+        })
+    })
+
+
 }
 
 app.whenReady().then(() => {
     createWindow()
+
+    ipcMain.on('servconvert', async (event, arg) => {
+
+        await new Promise((resolve) => { // wait
+            ytdl(`${arg}`)
+                .pipe(fs.createWriteStream('./download/video.mp4'))
+                .on('close', () => {
+                    event.returnValue = 'Video télécharger';
+                    resolve(); // finish
+                })
+        })
+
+    });
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
--- preload.js
+++ preload.js
@@ -1,9 +1,34 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   const ipc = require('electron').ipcRenderer;
+  var received = document.getElementById('received')
+  var infoytid = document.getElementById('infoyt')
 
-  document.getElementById('').addEventListener('click', () => {
-    ipc.send('audiostart');
+  document.getElementById('convert').addEventListener('click', () => {
+    var input = document.getElementById("ytlink");
+    var inputvalue = input.value
+
+    var infoyt = ipc.sendSync("servinfo", inputvalue);
+
+    infoytid.innerHTML = infoyt;
+
+    var data = ipc.sendSync('servconvert', (input.value));
+
+    received.innerHTML = data;
+
+
   });
+
+  function timer() {
+    var texttimer = document.getElementById("timer");
+    var timer = 0;
+
+    setInterval(function () {
+      timer++;
+      texttimer.innerHTML = timer;
+      console.log(data)
+    }, 1000);
+  }
+
 
 })
